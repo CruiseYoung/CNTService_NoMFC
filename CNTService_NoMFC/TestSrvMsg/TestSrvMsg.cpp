74adb47b60fd1a9ac20598d73031e0fd2ec37482
CruiseYoung <CruiseYoung@outlook.com>
2014-10-26 17:06:14 +0800

    去掉注释，恢复源码

--- CNTService_NoMFC/TestSrvMsg/TestSrvMsg.cpp
+++ CNTService_NoMFC/TestSrvMsg/TestSrvMsg.cpp
@@ -1,18 +1,17 @@
 //#include "stdafx.h"
-//#include <windows.h>
-#if 0
+#include <windows.h>
+
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /*lpReserved*/)
 {
-  if (dwReason == DLL_PROCESS_ATTACH)
-    DisableThreadLibraryCalls(hInstance);
-  return TRUE;
+    if (dwReason == DLL_PROCESS_ATTACH)
+        DisableThreadLibraryCalls(hInstance);
+    return TRUE;
 }
-#endif
 
 //The following function is provided to ensure that a .lib file
 //is generated for TestSrvMsg. This is required as we are using
 //the build in Dependency settings in VC to ensure the message
 //dll is build before the NT service exe.
-//void __declspec(dllexport) Dummy()
-//{
-//}
+void __declspec(dllexport) Dummy()
+{
+}
